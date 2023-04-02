befa1111755681b54606794c2126b4ecc1d3facc
Anthony Urbina <aurbinava002@gmail.com>
2023-04-02 11:17:56 -0700

    saving prog

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
-import './App.css';
-import { PeazeSDK, SupportedNetwork } from '@peaze-labs/react';
-import { useState } from 'react';
-import { ethers, constants, providers } from 'ethers';
+import "./App.css";
+import { PeazeSDK, SupportedNetwork } from "@peaze-labs/react";
+import { useState } from "react";
+import { providers } from "ethers";
 
 const peaze = new PeazeSDK({
-  id: '13594200814735e136793d5ae7db2d69b05ad24542c4db1e68b11c93aa99fc64',
-  key: '4795e022ae923eb0e670db0d309789e08d55aac57f5237dfd8f5c3a242e6df48',
+  id: "13594200814735e136793d5ae7db2d69b05ad24542c4db1e68b11c93aa99fc64",
+  key: "4795e022ae923eb0e670db0d309789e08d55aac57f5237dfd8f5c3a242e6df48",
   network: {
-    chainId: SupportedNetwork.PolygonMumbai
-    }
+    chainId: SupportedNetwork.PolygonMumbai,
+  },
 });
 
-function App() {
-console.log(peaze)
+// Using Chain ID of 80001 for Polygon Mumbai
+const provider = new providers.AlchemyProvider(
+  80001,
+  "Guv_wE_gl8Up8_BfakHWwBjpl2roO6wh"
+);
+
+const App = () => {
   const [signer, setSigner] = useState();
 
-  // Using Chain ID of 80001 for Polygon Mumbai
-  const provider = new providers.AlchemyProvider(80001, "Guv_wE_gl8Up8_BfakHWwBjpl2roO6wh");
-
   const login = async () => {
-    const signer = await peaze.getSigner(provider)
-    setSigner(signer)
-  }
+    const signer = await peaze.getSigner(provider);
+    setSigner(signer);
+    console.log("signer:", signer);
+  };
 
   const logout = async () => {
-    setSigner()
-  }
+    setSigner(null);
+  };
 
   return (
     <div className="App">
-      <button className='button' onClick={!signer ? login : logout}>{!signer ? 'Sign in' : 'Sign out'}</button>
+      <button className="button" onClick={!signer ? login : logout}>
+        {!signer ? "Sign in" : "Sign out"}
+      </button>
     </div>
   );
-}
+};
 
 export default App;
